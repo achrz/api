b9cf165b5150f42046083d7716b9f92dcd09cdbb
achrz <achmadrizky486@gmail.com>
2024-01-09 16:59:20 +0700

    fix : curl agent

--- app/Models/TodosModel.php
+++ app/Models/TodosModel.php
@@ -12,18 +12,31 @@ class TodosModel extends Model
 
     public function getTodos()
     {
-        $client = \Config\Services::curlrequest();
-        $response = $client->request('GET', $this->baseURL, ['allow_redirects' => true]);
-        // echo ($response->getStatusCode());
-        // exit;
-        if ($response->getStatusCode() == 200) {
-            // Decode JSON response
-            $todos = json_decode($response->getBody(), true);
+        $curl = curl_init();
 
-            return $todos;
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://www.script.google.com/macros/s/AKfycbxfyFeMYI30GwWS1fc4sMQ5vBlFxVCyR4Od1fL30IjZfMr6x-M8A7-XFJD_3lsnRRGI/exec",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "Accept: */*",
+                "User-Agent: Thunder Client (https://www.thunderclient.com)"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
         } else {
-            // Handle error
-            return [];
+            echo $response;
         }
     }
 }
